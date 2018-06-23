115fb962fb0a59ec80768cf6dd399327c67e97c3
USMAN <usmangeo14@gmail.com>
2018-06-23 05:15:31 +0100

    Logo Improved

--- app/src/androidTest/java/com/example/usman/newawareness/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/usman/newawareness/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.usman.newawareness;
+package com.awareness.usman.newawareness;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -15,12 +15,12 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class awarenessInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.usman.newawareness", appContext.getPackageName());
+        assertEquals("com.awareness.usman.newawareness", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/example/usman/newawareness/ExampleUnitTest.java
+++ app/src/test/java/com/example/usman/newawareness/ExampleUnitTest.java
@@ -1,15 +1,15 @@
-package com.example.usman.newawareness;
+package com.awareness.usman.newawareness;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * awareness local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class awarenessUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
